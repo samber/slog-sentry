2d48ab734afc745e45b481dce88329353eb30acc
samber <samber@users.noreply.github.com>
2024-04-26 00:25:48 +0000

    bump v2.5.0

--- version.go
+++ version.go
@@ -3,4 +3,4 @@ package slogsentry
 const name = "samber/slog-sentry"
 
 // nolint
-const version = "VERSION" // replaced by .github/workflows/release.yml
+const version = "v2.5.0" // replaced by .github/workflows/release.yml
